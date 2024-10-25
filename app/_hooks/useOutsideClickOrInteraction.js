14eb2fbd2b334561fc17f7b0d1290d872a780ade
Gordianus <poszukiwacz.gordianus@gmail.com>
2024-10-25 20:17:10 +0200

    fix: update useOutsideClickOrInteraction hook

--- app/_hooks/useOutsideClickOrInteraction.js
+++ app/_hooks/useOutsideClickOrInteraction.js
@@ -8,21 +8,24 @@ export default function useOutsideClickOrInteraction(
   const ref = useRef();
 
   useEffect(() => {
+    const removeEvent = () =>
+      options.forEach((option) =>
+        document.removeEventListener(option, handleEvent, listenCapturing),
+      );
+
     function handleEvent(e) {
       if (ref.current && !ref.current.contains(e.target)) {
         callback();
+        removeEvent(); // Remove event listener after invoking the callback
       }
     }
 
-    options.map((option) =>
+    options.forEach((option) =>
       document.addEventListener(option, handleEvent, listenCapturing),
     );
 
-    return () =>
-      options.map((option) =>
-        document.removeEventListener(option, handleEvent, listenCapturing),
-      );
-  }, [callback, ref, options]);
+    return () => removeEvent(); // Cleanup event listeners on component unmount
+  }, [callback, options, listenCapturing]);
 
   return ref;
 }
